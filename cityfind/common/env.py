24c12c30c7216830fcc22f7215f9f614edc8a3a7
yiocardamone <155022426+yiocardamone@users.noreply.github.com>
2024-02-12 00:11:45 +0300

    Update env.py

--- cityfind/common/env.py
+++ cityfind/common/env.py
@@ -7,5 +7,11 @@ class EnvironmentVariable(Enum):
     LOGGING_RELATIVE_PATH = "CITYFIND_LOGGING_INI_PATH"
 
 
+_DEFAULTS = {
+    EnvironmentVariable.CONFIG_RELATIVE_PATH.value: "configs/config.yaml",
+    EnvironmentVariable.LOGGING_RELATIVE_PATH.value: "configs/logging.ini"
+}
+
+
 def load_env_variable(variable: EnvironmentVariable) -> str | None:
-    return os.environ.get(variable.value)
+    return os.environ.get(variable.value, _DEFAULTS.get(variable.value))
